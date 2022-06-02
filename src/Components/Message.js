c1eb76f31c81835f5bab822a6406c48aa6b409b6
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-06-02 21:25:05 +0530

    components folder add on

---
+++ src/Components/Message.js
@@ -0,0 +1,40 @@
+//messaging section
+
+import React from "react";
+import styled from "styled-components";
+
+function Message({ message, timestamp, user, userImage }) {
+  return (
+    <MessageContainer>
+      <img src={userImage} alt="" />
+      <MessageInfo>
+        <h4>
+          {user}
+          {""}
+          <span>{new Date(timestamp?.toDate()).toUTCString()}</span>
+        </h4>
+        <p>{message}</p>
+      </MessageInfo>
+    </MessageContainer>
+  );
+}
+
+export default Message;
+const MessageContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  > img {
+    height: 50px;
+    border-radius: 45px;
+  }
+`;
+const MessageInfo = styled.div`
+  padding-left: 10px;
+  > h4 {
+    color: gray;
+    font-weight: 300;
+    margin-left: 4px;
+    font-size: 10px;
+  }
+`;
